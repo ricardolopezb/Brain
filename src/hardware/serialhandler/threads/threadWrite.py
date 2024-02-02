ed2da17636b8f85193b8f3f7650ad3ce315bf320
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-02-01 21:16:44 -0300

    test STS message receiver

--- src/hardware/serialhandler/threads/threadWrite.py
+++ src/hardware/serialhandler/threads/threadWrite.py
@@ -163,7 +163,7 @@ class threadWrite(ThreadWithStop):
                         self.serialCom.write(command_msg.encode("ascii"))
                         self.logFile.write(command_msg)
                 if self.running:
-                    print("SENDING DATA TO SERIAL")
+
                     if self.pipeRecvBreak.poll():
                         message = self.pipeRecvBreak.recv()
                         command = {"action": "1", "speed": float(message["value"])}
@@ -183,6 +183,7 @@ class threadWrite(ThreadWithStop):
                         self.serialCom.write(command_msg.encode("ascii"))
                         self.logFile.write(command_msg)
                     elif self.pipeRecvControl.poll():
+                        print("SENDING DATA TO SERIAL")
                         message = self.pipeRecvControl.recv()
                         command = {
                             "action": "9",
