558c2ddcfaaff540c34c5af7bc270ebcb7a07135
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-02-27 17:42:22 -0300

    remove test loop

--- src/hardware/camera/threads/threadCamera.py
+++ src/hardware/camera/threads/threadCamera.py
@@ -161,7 +161,7 @@ class threadCamera(ThreadWithStop):
                     "lores"
                 )  # Will capture an array that can be used by OpenCV library
                 request2 = request2[:360, :]
-                steering_value = self.lane_detector.get_steering_angle(request2)
+                steering_value = self.lane_detector.get_steering_angle(request)
                 print("***************** STEERING VALUE", steering_value)
                 _, encoded_img = cv2.imencode(".jpg", request2)
                 _, encoded_big_img = cv2.imencode(".jpg", request)
