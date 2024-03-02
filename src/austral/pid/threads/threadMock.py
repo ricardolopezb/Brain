f5caf8b9f0f5ed44c158640d00caf3ff4da1ab1b
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-03-02 01:51:00 -0300

    testing sign execution

--- src/austral/pid/threads/threadMock.py
+++ src/austral/pid/threads/threadMock.py
@@ -54,7 +54,7 @@ class threadMock(ThreadWithStop):
         self.pipeRecvSteeringCalculation = pipeRecvSteeringCalculation
         self.pipeSendSteeringCalculation = pipeSendSteeringCalculation
         self.subscribe()
-        speed = 30
+        speed = 5
         time.sleep(1)
         self.queuesList[SpeedMotor.Queue.value].put({
             "Owner": SpeedMotor.Owner.value,
