d60e02e28e536bf06ad1c3001e0e123bf79cfc71
Alejo Demitropulos <alejodemitropulos@Alejos-MacBook-Pro.local>
2024-03-03 15:30:27 -0300

    change update endpoint

---
+++ src/hardware/camera/test.py
@@ -0,0 +1,36 @@
+import cv2
+import torch
+import time
+import numpy as np
+
+# Cargar el modelo
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='.\\yolov5\\runs\\train\\exp4\\weights\\best.pt', force_reload=True)
+
+def capture_and_process_images(model):
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("No se pudo abrir la cámara web.")
+        return
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: no se pudo capturar la imagen de la cámara web.")
+                break
+            # Procesar la imagen con el modelo
+            results = model(frame)
+
+            # Mostrar la imagen procesada
+            cv2.imshow('Processed Image', cv2.cvtColor(np.squeeze(results.render()), cv2.COLOR_BGR2RGB))
+
+            # Esperar 5 segundos (5000 milisegundos)
+            if cv2.waitKey(1000) & 0xFF == ord('q'):
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+# Ejemplo de uso
+capture_and_process_images(model)
