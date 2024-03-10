d0f451f02149b3ec18102753d38152c8a62278ac
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-03-10 08:40:33 -0300

    new marcos algorithm

--- src/austral/api/data_sender.py
+++ src/austral/api/data_sender.py
@@ -4,4 +4,4 @@ import requests
 class DataSender:
     @staticmethod
     def send(endpoint, value):
-        response = requests.post(f'http://192.168.0.101:5000/{endpoint}', json=value)
+        response = requests.post(f'http://192.168.0.102:5000/{endpoint}', json=value)
