2df8a57a0bdad8877e63dca8f8b8483186ca4f4b
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-03-10 10:17:34 -0300

    new marcos algorithm

--- src/austral/api/data_sender.py
+++ src/austral/api/data_sender.py
@@ -1,7 +1,9 @@
 import requests
 
+from src.austral.configs import MODEL_API_URL
+
 
 class DataSender:
     @staticmethod
     def send(endpoint, value):
-        response = requests.post(f'http://192.168.0.102:5000/{endpoint}', json=value)
+        response = requests.post(f'{MODEL_API_URL}/{endpoint}', json=value)
--- src/austral/signals/model_request_sender.py
+++ src/austral/signals/model_request_sender.py
@@ -1,10 +1,12 @@
 import requests
 
+from src.austral.configs import MODEL_API_URL
+
 
 # This would be called in the threadCamera when encoding the frame into .jpg
 class ModelRequestSender:
     def send(self, imdata, lookup_sign):
-        response = requests.put(f'http://192.168.0.101:5000/upload/{lookup_sign}', data=imdata.tobytes())
+        response = requests.put(f'{MODEL_API_URL}/upload/{lookup_sign}', data=imdata.tobytes())
         if response.status_code == 200:
             json_response = response.json()
             return json_response
