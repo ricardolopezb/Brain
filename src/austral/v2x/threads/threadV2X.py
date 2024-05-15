2441c36a215514fbda817894d191052a639a7818
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-05-15 14:56:51 +0300

    semaphores

--- src/austral/v2x/threads/threadV2X.py
+++ src/austral/v2x/threads/threadV2X.py
@@ -1,12 +1,14 @@
 import os
 import threading
 from multiprocessing import Pipe
+
+from src.austral.configs import MY_CAR_ID, V2X_SEMAPHORE_INFLUENCE_RADIUS, BASE_SPEED, V2X_SEMAPHORE_COOLDOWN
 from src.hardware.serialhandler.threads.messageconverter import MessageConverter
 from src.templates.threadwithstop import ThreadWithStop
 from src.utils.messages.allMessages import (
     SignalRunning,
     Cars,
-    Semaphores
+    Semaphores, SpeedMotor
 )
 import time
 
@@ -35,6 +37,12 @@ class threadV2X(ThreadWithStop):
         pipeRecvSemaphores, pipeSendSemaphores = Pipe(duplex=False)
         self.pipeRecvSemaphores = pipeRecvSemaphores
         self.pipeSendSemaphores = pipeSendSemaphores
+
+        self.my_car_id = MY_CAR_ID
+        self.my_previous_coordinates = (0, 0)
+        self.my_current_coordinates = (0, 0)
+
+        self.last_time_semaphore_action_taken = time.time()
 
         self.logging_filename = f'{int(time.time())}.txt'
         self.subscribe()
@@ -66,7 +74,6 @@ class threadV2X(ThreadWithStop):
 
     # ===================================== RUN ==========================================
     def run(self):
-
         """In this function we check if we got the enable engine signal. After we got it we will start getting messages from raspberry PI. It will transform them into NUCLEO commands and send them."""
 
         while self._running:
@@ -76,16 +83,63 @@ class threadV2X(ThreadWithStop):
                     # print(f"Received CARS -> {message}")
                     x = message['value']['x']
                     y = message['value']['y']
-                    mode = 'a' if os.path.exists(self.logging_filename) else 'w'
-                    with open(self.logging_filename, mode) as file:
-                        file.write(f'{x};{y}\n')
+                    if message['value']['id'] == self.my_car_id:
+                        self.log_my_coordinates(x, y)
+                        self.my_previous_coordinates = self.my_current_coordinates
+                        self.my_current_coordinates = (x, y)
 
-                if self.pipeRecvSemaphores.poll():
+                if self.pipeRecvSemaphores.poll() and time.time() - self.last_time_semaphore_action_taken >= V2X_SEMAPHORE_COOLDOWN:
                     message = self.pipeRecvSemaphores.recv()
                     print(f"Received SEMAPHORES -> {message}")
+                    semaphore_x = message['value']['x']
+                    semaphore_y = message['value']['y']
+
+                    if self.is_not_within_semaphore_range(semaphore_x, semaphore_y):
+                        return
+                    if self.is_moving_away_from_semaphore(semaphore_x, semaphore_y):
+                        return
+                    semaphore_state = message['value']['state']
+                    if semaphore_state == 'green':
+                        self.accelerate()
+                    if semaphore_state == 'red':
+                        self.brake()
+                    self.last_time_semaphore_action_taken = time.time()
 
             except Exception as e:
                 print(e)
+
+    def is_not_within_semaphore_range(self, semaphore_x, semaphore_y):
+        return self.calculate_distance(semaphore_x, semaphore_y, self.my_current_coordinates[0],
+                                       self.my_current_coordinates[1]) > V2X_SEMAPHORE_INFLUENCE_RADIUS
+
+    def is_moving_away_from_semaphore(self, semaphore_x, semaphore_y):
+        return self.calculate_distance(semaphore_x, semaphore_y, self.my_current_coordinates[0], self.my_current_coordinates[1]) > self.calculate_distance(semaphore_x, semaphore_y, self.my_previous_coordinates[0], self.my_previous_coordinates[1])
+
+    def brake(self):
+        print("Found RED LIGHT, braking")
+        self.queuesList[SpeedMotor.Queue.value].put({
+            "Owner": SpeedMotor.Owner.value,
+            "msgID": SpeedMotor.msgID.value,
+            "msgType": SpeedMotor.msgType.value,
+            "msgValue": 0
+        })
+
+    def accelerate(self):
+        print("Found GREEN LIGHT, accelerating")
+        self.queuesList[SpeedMotor.Queue.value].put({
+            "Owner": SpeedMotor.Owner.value,
+            "msgID": SpeedMotor.msgID.value,
+            "msgType": SpeedMotor.msgType.value,
+            "msgValue": BASE_SPEED
+        })
+
+    def calculate_distance(self, target_x, target_y, base_x, base_y):
+        return ((target_x - base_x) ** 2 + (target_y - base_y) ** 2) ** 0.5
+
+    def log_my_coordinates(self, x, y):
+        mode = 'a' if os.path.exists(self.logging_filename) else 'w'
+        with open(self.logging_filename, mode) as file:
+            file.write(f'{x};{y}\n')
 
     # ==================================== START =========================================
     def start(self):
